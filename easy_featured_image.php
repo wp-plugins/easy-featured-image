a0ec55c2cde53892a8cd55dc73f0cf1cce292775
RPG84 <RPG84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-28 01:54:45 +0000

    git-svn-id: https://plugins.svn.wordpress.org/easy-featured-image/trunk@338029 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- easy_featured_image.php
+++ easy_featured_image.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Easy Featured Image
 Plugin URI: http://wordpress.org/extend/plugins/easy-featured-image/
-Version: 1.0
+Version: 1.1
 Description: This plugin enables editting the featured images on the edit.php in the admin menu. You will need to put this in your functions.php to enable featured images:<?php add_theme_support('post-thumbnails'); ?>
 Author: Robert Braam
 Author URI: http://randommusicvideos.com
@@ -13,17 +13,25 @@ function show_featured_image_column( $defaults ) {
     	return $defaults;
 }
 add_filter('manage_posts_columns', 'show_featured_image_column');
+add_filter('manage_pages_columns', 'show_featured_image_column');
 
 
-function edit_featured_images($column_name, $id, $post_id) {
+function edit_featured_images($column_name, $id) {
 global $post;
     if( $column_name == 'featuredimage' && has_post_thumbnail()) {
         the_post_thumbnail( array(100,100) );
-}
-    if( $column_name == 'featuredimage'){
+	/*
+	$ajax_nonce = wp_create_nonce($post->ID);
         echo "<div class=\"row-actions\"><span class='edit'>";
-        echo '<a href="'. admin_url() . 'media-upload.php?post_id='.$post->ID.'&type=image&TB_iframe=1&width=640&height=500" target="_blank">' . __( 'Edit' ) . '</a>';
+	echo '<a href="#" id="remove-post-thumbnail" onclick="WPRemoveThumbnail(\''.$ajax_nonce.'\');return false;">Remove featured image</a>';
+        echo "</span></div>";
+	*/
+}
+    if( $column_name == 'featuredimage' && !has_post_thumbnail()){
+        echo "<div class=\"row-actions\"><span class='edit'>";
+        echo '<a href="'. admin_url() . 'media-upload.php?post_id='.$post->ID.'&type=image&TB_iframe=1&width=640&height=500" target="_blank">' . __( 'Add' ) . '</a>';
         echo "</span></div>";
 }}
 add_action('manage_posts_custom_column', 'edit_featured_images', 10, 2);
+add_action('manage_pages_custom_column', 'edit_featured_images', 10, 2);
 ?>
--- readme.txt
+++ readme.txt
@@ -3,6 +3,7 @@ Contributors: RPG84
 Tags: featured image, admin, easy, edit
 Requires at least: 3.0
 Tested up to: 3.0.4
+Stable tag: 1.1
 
 Easily edit and see featured images on your post edit screen.
 
@@ -11,15 +12,18 @@ On edit.php you will be able to see the currently used featured image for a post
 
 == Installation ==
 
-1) Add the following in your functions.php to enable featured images:<?php add_theme_support('post-thumbnails'); ?>
-2) Place easy_featured_images into your plugin directoy, and activate it.
-3) Goto your post edit screen to see your featured images
+1. Add the following in your functions.php to enable featured images:<?php add_theme_support('post-thumbnails'); ?>
+1. Place easy_featured_images into your plugin directoy, and activate it.
+1. Goto your post edit screen to see your featured images
 
 == Frequently Asked Questions ==
 = I don't see the tumbnails in my theme =
 You need to put `<?php the_post_thumbnail(array( 100,100 )); ?>` somewhere in your theme
 
 == Changelog ==
+= 1.1 =
+Fixed a minor error 
+Added it for pages too
 
 = 1.0 =
 First Release
